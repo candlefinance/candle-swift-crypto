da2050cafb11d83f953d7933f1e5d0fe02b99b32
Nikolai Wotton <wottonn@gmail.com>
2026-01-22 07:17:38 -0500

    prefixing candle- to repo

--- Package.swift
+++ Package.swift
@@ -200,7 +200,7 @@ let package = Package(
 // Switch between local and remote dependencies depending on an environment variable
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     package.dependencies += [
-        .package(name: "candle-swift-asn1", url: "https://github.com/candlefinance/swift-asn1.git", branch: "fix-candle-1.3.2")
+        .package(name: "candle-swift-asn1", url: "https://github.com/candlefinance/candle-swift-asn1.git", branch: "fix-candle-1.3.2")
     ]
 } else {
     package.dependencies += [
